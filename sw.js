cf1c7c07d0a75e54f3bb2a8565c61eb793965096
toniad74 <toniad74@gmail.com>
2026-02-11 21:01:06 +0100

    v1.7.2 - Añadida nueva clave API de YouTube al pool

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'amaya-music-v31';
-// SW Version: 1.7.1 - Time: 20:56 (GMT+1) - Auto-actualización
+const CACHE_NAME = 'amaya-music-v32';
+// SW Version: 1.7.2 - Time: 21:01 (GMT+1) - Auto-actualización
 const urlsToCache = [
     './',
     './index.html',
