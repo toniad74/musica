78448d77f2f42984a371c2588f05023756909e88
toniad74 <toniad74@gmail.com>
2026-02-12 15:22:44 +0100

    v1.7.32 - Auto-update version increment

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'amaya-music-v61';
-// SW Version: 1.7.31 - Time: 15:11 (GMT+1) - Auto-actualización
+const CACHE_NAME = 'amaya-music-v62';
+// SW Version: 1.7.32 - Time: 15:22 (GMT+1) - Auto-actualización
 const urlsToCache = [
     './',
     './index.html',
